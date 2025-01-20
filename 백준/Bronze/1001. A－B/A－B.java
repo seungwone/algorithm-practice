72510698942bd3f563ded1ef70cb89571de42fb7
Seungwon Lee <aahhll654@gmail.com>
2025-01-21 01:40:53 +0900

    [Bronze V] Title: A-B, Time: 92 ms, Memory: 12868 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -4,5 +4,6 @@ class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println((sc.nextInt()-sc.nextInt()));
+        sc.close();
     }
 }
