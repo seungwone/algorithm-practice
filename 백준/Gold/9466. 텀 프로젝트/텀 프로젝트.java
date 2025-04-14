ee9c602eb12214cad5e1c2ed6c18ffb3df262bb9
Seungwon Lee <aahhll654@gmail.com>
2025-04-14 21:45:10 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1824 ms, Memory: 313772 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -10,8 +10,10 @@ class Main {
         for (int tc = 0; tc < T; tc++) {
             int n = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int answer = 0;
             int[] graph = new int[n + 1];
             int[] isSelected = new int[n + 1];
+            boolean[] isVisited = new boolean[n + 1];
             
             for (int i = 1; i <= n; i++)
                 graph[i] = Integer.parseInt(st.nextToken());
@@ -20,28 +22,18 @@ class Main {
                 if (isSelected[i] != 0)
                     continue;
 
-                Set<Integer> isVisited = new HashSet<>();
                 List<Integer> list = new ArrayList<>();
-                isVisited.add(i);
+                isVisited[i] = true;
                 list.add(i);
                 int next = graph[i];
-                while (!isVisited.contains(next) && isSelected[next] == 0) {
-                    isVisited.add(next);
+                while (!isVisited[next]) {
+                    isVisited[next] = true;
                     list.add(next);
                     next = graph[next];
                 }
 
-                if (isSelected[next] != 0) {
-                    for (int e : list) {
-                        if (e == next)
-                            break;
-                        isSelected[e] = -1;
-                    }
-                    continue;
-                }
-
                 if (i == next) {
-                    for (int e : isVisited)
+                    for (int e : list)
                         isSelected[e] = 1;
                 }
                 else {
@@ -50,14 +42,11 @@ class Main {
                         if (e == next)
                             flag = 1;
                         isSelected[e] = flag;
+                        if (flag == -1)
+                            answer++;
                     }
                 }
             }
-
-            int answer = 0;
-            for (int i = 1; i <= n; i++)
-                if (isSelected[i] == -1)
-                    answer++;
             
             sb.append(answer + "\n");
         }
