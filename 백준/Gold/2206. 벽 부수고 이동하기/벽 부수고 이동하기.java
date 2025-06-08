498b1dd2167a45e49904e1892cea8a298e3019a6
Seungwon Lee <aahhll654@gmail.com>
2025-06-09 01:20:03 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 816 ms, Memory: 163752 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        boolean[][] map = new boolean[N + 1][M + 1];
+        int[][] isVisitedWithChance = new int[N + 1][M + 1];
+        int[][] isVisitedWithNoChance = new int[N + 1][M + 1];
+        int[] di = {-1, 1, 0, 0};
+        int[] dj = {0, 0, -1, 1};
+        int answer = -1;
+
+        for (int i = 1; i <= N; i++) {
+            String temp = br.readLine();
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = temp.charAt(j - 1) == '1';
+                isVisitedWithChance[i][j] = Integer.MAX_VALUE;
+                isVisitedWithNoChance[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        Deque<Integer[]> dq = new ArrayDeque<>();
+        dq.add(new Integer[] {1, 1, 1, 1});
+        isVisitedWithChance[1][1] = 1;
+
+        while (!dq.isEmpty()) {
+            Integer[] ele = dq.remove();
+            int curI = ele[0];
+            int curJ = ele[1];
+            int cnt = ele[2];
+            int chance = ele[3];
+
+            if (curI == N && curJ == M) {
+                answer = cnt;
+                break;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nextI = curI + di[d];
+                int nextJ = curJ + dj[d];
+
+                if (nextI >= 1 && nextI <= N && nextJ >= 1 && nextJ <= M) {
+
+                    if (map[nextI][nextJ] == true) {
+                        if (chance == 1 && isVisitedWithNoChance[nextI][nextJ] > cnt + 1) {
+                            isVisitedWithNoChance[nextI][nextJ] = cnt + 1;
+                            dq.add(new Integer[] {nextI, nextJ, cnt + 1, 0});
+                        }
+                    }
+                    else {
+                        if (chance == 1 && isVisitedWithChance[nextI][nextJ] > cnt + 1) {
+                            isVisitedWithChance[nextI][nextJ] = cnt + 1;
+                            dq.add(new Integer[] {nextI, nextJ, cnt + 1, chance});
+                        }
+                        else if (chance == 0 && isVisitedWithNoChance[nextI][nextJ] > cnt + 1 && isVisitedWithChance[nextI][nextJ] > cnt + 1) {
+                            isVisitedWithNoChance[nextI][nextJ] = cnt + 1;
+                            dq.add(new Integer[] {nextI, nextJ, cnt + 1, chance});
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+}
