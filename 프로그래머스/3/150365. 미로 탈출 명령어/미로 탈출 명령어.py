fb75b0bacb431ca4a94fe949d1136618bf620ee4
Seungwon Lee <aahhll654@gmail.com>
2025-10-21 14:56:32 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 13.43 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -3,8 +3,6 @@ def solution(n, m, x, y, r, c, k):
     di = [-1, 0, 0, 1]
     dj = [0, 1, -1, 0]
     dd = ['u', 'r', 'l', 'd']
-    is_visited = [[set() for _ in range(m + 1)] for _ in range(n + 1)]
-    is_visited[x][y].add('')
     stack = []
     stack.append((x, y, ''))
     
@@ -14,14 +12,10 @@ def solution(n, m, x, y, r, c, k):
         s_path = abs(cur_i - r) + abs(cur_j - c)
         
         if cur_i == r and cur_j == c and str_len == k:
-            if cur_str < answer or answer == 'impossible':
-                answer = cur_str
-                continue
+            answer = cur_str
+            break
         
         if str_len + s_path > k or (k - (str_len + s_path)) % 2 == 1:
-            continue
-            
-        if answer != 'impossible' and str_len > 0 and cur_str > answer:
             continue
         
         for d in range(4):
@@ -29,8 +23,7 @@ def solution(n, m, x, y, r, c, k):
             next_j = cur_j + dj[d]
             next_str = cur_str + dd[d]
             
-            if 1 <= next_i <= n and 1 <= next_j <= m and next_str not in is_visited[next_i][next_j]:
-                is_visited[next_i][next_j].add(next_str)
+            if 1 <= next_i <= n and 1 <= next_j <= m:
                 stack.append((next_i, next_j, next_str))
     
     return answer
